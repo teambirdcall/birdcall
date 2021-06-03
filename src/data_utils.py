9c9eda1c756c8c4e3d0faa0b39cdc35155933eab
ananya-1401 <76123942+ananya-1401@users.noreply.github.com>
2021-06-03 22:40:25 +0530

    Add files via upload

---
+++ src/data_utils.py
@@ -0,0 +1,9 @@
+import os
+
+ROOT_PATH = os.path.dirname(os.path.abspath('.../')) 
+AUDIODATA_DIR = os.path.join(ROOT_PATH,'audiodata')
+RESULT_DIR = os.path.join(ROOT_PATH, 'result')
+MEL_DIR = os.path.join(ROOT_PATH, 'melresults')
+PITCH_DIR = os.path.join(ROOT_PATH, 'pitch_change')
+TIME_DIR = os.path.join(ROOT_PATH, 'time_change')
+
---
+++ src/model_utils_.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  3 20:49:54 2021
+
+@author: Ananya
+"""
+
