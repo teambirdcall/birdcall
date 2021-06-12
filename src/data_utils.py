c4092413f702812dd5318417b982fedf4838e961
ananya-1401 <76123942+ananya-1401@users.noreply.github.com>
2021-06-12 14:37:47 +0530

    Update data_utils.py
    I have updated my data_utils file

--- src/data_utils.py
+++ src/data_utils.py
@@ -12,45 +12,39 @@ PITCH_DIR = os.path.join(ROOT_PATH, 'pitch_change') #augmented chunk=pitch
 TIME_DIR = os.path.join(ROOT_PATH, 'time_change')# augmented chunks=time
 
 meta=pd.read_csv(os.path.join(os.path.abspath(MEL_DIR)+"/"+"mel_meta.csv"))
-X=[]
-Y=[]
-for index_num,row in (meta.iterrows()):
-    filename_image=os.path.join(os.path.abspath(MEL_DIR)+"/"+str(row['Class_Label'])+
-                                "/"+str(row["Image_Name"]))
-    img=cv2.imread(filename_image)
-        #resizing image
-    img= cv2.resize(img, (0, 0), fx = 0.5, fy = 0.5)
-    X.append(img/255.0)
-    Y.append(row['Class_Label'])
- X=np.array(X)
 def encode(y):
-    #encoding 
+    #one hot encoding
     y=pd.get_dummies(data=y,columns=['Class_Label'])
     return y
 
 def train_test_val_split(test_size, validation_size): 
-    # create train, validation and test split
+    # splits the data into  train, validation and test 
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size)
     X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, 
                                                                     test_size=validation_size)
     return X_train, X_validation, X_test, y_train,y_validation, y_test
 
-def preprocessing_out_in(y_train,y_test,y_validation,X_train):
-    y_train=y_train.values.tolist()
-    y_train=np.array(y_train)
-    y_test=y_test.values.tolist()
-    y_test=np.array(y_test)
-    y_validation=y_validation.values.tolist()
-    y_validation=np.array(y_validation)
-    X_train = X_train.reshape((-1,X.shape[1],X.shape[2],X.shape[3]))
-    return y_train,y_validation,y_test,X_train
+if __name__=="__main__":
+    X=[]
+    Y=[]
+    for index_num,row in (meta.iterrows()):
+        filename_image=os.path.join(os.path.abspath(MEL_DIR)+"/"+str(row['Class_Label'])+
+                                "/"+str(row["Image_Name"]))
+        img=cv2.imread(filename_image)
+        #resizing image
+        img= cv2.resize(img, (0, 0), fx = 0.5, fy = 0.5)
+        X.append(img/255.0)
+        Y.append(row['Class_Label'])
 
-
-Y=pd.DataFrame(Y)
-Y.columns=['Class_Label']  
+X=np.array(X)# converting the list to array -- for feeding in CNN
+X=X.reshape((-1,X.shape[1],X.shape[2],X.shape[3]))#reshaping as image's dimension is 4D
+Y=pd.DataFrame(Y)#for get_dummies, we need to feed a dataframe
+Y.columns=['Class_Label']
 Y=encode(Y)
+#converting it back to numpy array 
+Y=Y.values.tolist()
+Y=np.array(Y)
+#to split the dataset
 X_train, X_validation, X_test, y_train, y_validation, y_test= train_test_val_split(0.15, 0.15)
-preprocessing_out_in(y_train,y_test,y_validation,X_train)
 
-
-
+    
