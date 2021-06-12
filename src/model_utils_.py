97ab1ceddddd7018eb37ac95e2d0c3b7b57d8375
ananya-1401 <76123942+ananya-1401@users.noreply.github.com>
2021-06-12 23:08:22 +0530

    Update model_utils_.py
    I have updated model_utils_

--- src/model_utils_.py
+++ src/model_utils_.py
@@ -1,72 +1,131 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from data_utils import *
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from xeno_df import *
 
-class build_CNN:
-    
-    def __init__(self,X_train,y_train,X_test,y_test,lr,epoch,filters,
-                 kernel,user_layers,inp):
+class BirdCall_CNN:
+    def __init__(self,data):
+        self.data=data
+        self.model=None
         
-        self.model = None
-        self.X_train = X_train
-        self.y_train = y_train
-        self.X_test = X_test
-        self.y_test = y_test
-        self.lr = lr
-        self.epoch = epoch
-        self.filters = filters
-        self.kernel = kernel
-        self.optimiser = None
-        self.evaluate_model = None
-        self.user_layers = user_layers
-        self.inp = inp
-        self.f = None
-        
-    def model_CNN(self):
-        self.f=0
+    def model_CNN(self,inp): 
+        """
+        Parameters
+        ----------
+        filters : a list stored in configure.json
+            number of filters to feed to CNN
+        kernel :tuple
+            width x height of the filter mask
+        user_layers : a list stored in configure.json
+            number of layers in CNN
+        inp : tuple
+            stores input size of mel data:X
+        Returns
+        -------
+         CNN model 
+        """
+        f=0
         self.model=keras.Sequential()
-    
         #----input layer--- 
-        self.model.add(tf.keras.layers.Conv2D(self.filters[self.f],self.kernel, 
-                                          activation='relu',input_shape=self.inp))
-        self.model.add(tf.keras.layers.MaxPooling2D(self.filters[self.f],self.kernel,
+        self.data['kernel size']=tuple(self.data['kernel size'])
+        self.model.add(tf.keras.layers.Conv2D(self.data['filters'][f],
+                                              self.data['kernel size'], 
+                                          activation='relu',input_shape=inp))
+        self.model.add(tf.keras.layers.MaxPooling2D(self.data['filters'][f],
+                                                    self.data['kernel size'],
                                                     padding='same'))
         self.model.add(tf.keras.layers.Dropout(0.25))
-    
+        
         #----hidden layers---
         # used the loop for the extra hidden layers in CNN other than input,flatten,output
-        for i in range(self.user_layers-1):
-            self.f=self.f+1
-            self.model.add(tf.keras.layers.Conv2D(self.filters[self.f],self.kernel, 
+        for i in range(self.data['number of layers'][0]-1):
+            f=f+1
+            self.model.add(tf.keras.layers.Conv2D(self.data['filters'][f],
+                                                  self.data['kernel size'], 
                                                   activation='relu'))
-            self.model.add(tf.keras.layers.MaxPooling2D(self.filters[self.f],self.kernel,
+            self.model.add(tf.keras.layers.MaxPooling2D(self.data['filters'][f],
+                                                        self.data['kernel size'],
                                                         padding='same'))
             self.model.add(tf.keras.layers.Dropout(0.25))
-        
+            
         #Flatten Out and feed it into Dense layer
         self.model.add(tf.keras.layers.Flatten())
-        self.model.add(tf.keras.layers.Dense(self.filters[self.f],
+        self.model.add(tf.keras.layers.Dense(self.data['filters'][f],
                                              activation='relu'))
         self.model.add(tf.keras.layers.Dropout(0.30))
-    
-        #output layer
-        self.model.add(keras.layers.Dense(8, activation='softmax'))
         
+        #output layer
+        self.model.add(keras.layers.Dense(7, activation='softmax'))
         return self.model
            
     def compile_CNN(self):
-        self.optimiser = keras.optimizers.Adam(learning_rate=self.lr)
-        self.model.compile(optimizer=self.optimiser,
+        #compiles model using Adam optimiser
+        optimiser = keras.optimizers.Adam(learning_rate=self.data['learning rate'][0])
+        self.model.compile(optimizer=optimiser,
                            loss='categorical_crossentropy',
                            metrics=['accuracy'])
     def CNN_summary(self):
+        #gives the model summary
         print(self.model.summary())
     
-    def fit_CNN(self):
-        print(self.model.fit(self.X_train, self.y_train, epochs=self.epoch))
+    def fit_CNN(self,X_train,y_train):
+        """
+        Parameters
+        ----------
+        X_train : array
+            training mel dataset.
+        y_train : array
+            Output array of Class Label .
+        epoch : a list stored in configure.json
+            number of passes of entire training dataset
+
+        Returns
+        -------
+        None.
         
-    def evaluate_CNN(self):
-        self.evaluate_model=self.model.evaluate(self.X_test,self.y_test)
-        print("Test Data evaluation:",self.evaluate_model)
+        fits the training dataset on the model,
+        plots the loss using history callback.
+        saves the model if current_accuracy > prev_accuracy
+        
+        """
+        history = self.model.fit(X_train, y_train,epochs=self.data['epochs'][2])
+        loss_train = history.history['loss'] # gets the entire history of loss
+        accuracy_train=history.history['accuracy']# gets the entire history of accuracy
+        plt.plot(loss_train)
+        plt.title('Training loss')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.show()
+        
+        max_acc =accuracy_train[0]
+        for value in accuracy_train:
+            if value > max_acc:
+                max_acc= value
+                # save the model whenever accuracy > prev_accuracy
+                self.model.save("my_model")
+                print("Model saved at accuracy=",max_acc)
+   
+    
+    def evaluate_CNN(self,X_test,y_test):
+        """
+        Parameters
+        ----------
+        X_test :array
+            test mel dataset
+        y_test : array
+            test output dataset: Class Label
+
+        Returns
+        -------
+        None.
+        
+        evaluates the model on test data.
+
+        """
+        evaluate_model=self.model.evaluate(X_test,y_test)
+        print("Test Data evaluation:",evaluate_model)
         
