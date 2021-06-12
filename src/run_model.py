3cc76a342765d0845089f9b1f4df332641052e7d
ananya-1401 <76123942+ananya-1401@users.noreply.github.com>
2021-06-12 23:10:43 +0530

    Update run_model.py
    I have updated run_model

--- src/run_model.py
+++ src/run_model.py
@@ -6,21 +6,10 @@ inp=(X.shape[1],X.shape[2],X.shape[3])
 with open('configure.json') as f:
   data = json.load(f)
 
-call_CNN=build_CNN(X_train,y_train,X_test,y_test,
-                   data['learning rate'][1],
-                   data['epochs'][2],
-                   data['filters'],
-                 tuple(data['kernel size']),
-                 data['number of layers'][0],inp)
-#model builder
-call_CNN.model_CNN()
-#compiles model using optimiser='adam'
+call_CNN=BirdCall_CNN(data)
+
+call_CNN.model_CNN(inp) #model builder
 call_CNN.compile_CNN()
-# gets the model architecture/summary
 call_CNN.CNN_summary()
-#Fits the model into the data
-call_CNN.fit_CNN()
-# evaluates accuracy for test data
-call_CNN.evaluate_CNN()
-
-
+call_CNN.fit_CNN(X_train,y_train)
+call_CNN.evaluate_CNN(X_test,y_test)
